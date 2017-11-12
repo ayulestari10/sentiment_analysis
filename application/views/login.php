51a0a0ac658503c2f7cbdd07cdd2189ce50accac
apriliaaachi <achiaprilia.aa@gmail.com>
2017-11-12 19:01:31 +0700

    login view

--- application/views/login.php
+++ application/views/login.php
@@ -12,16 +12,11 @@
     <title><?=$title?></title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="<?=base_url('assets/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="<?=base_url('assets/vendor/metisMenu/metisMenu.min.css')?>" rel="stylesheet">
+    <link href="<?=base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="<?=base_url('assets/dist/css/sb-admin-2.css')?>" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/login/stylelogin.css')?>">
 
-    <!-- Custom Fonts -->
-    <link href="<?=base_url('assets/vendor/font-awesome/css/font-awesome.min.css')?>" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -34,35 +29,17 @@
 
 <body>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Please Sign In</h3>
-                    </div>
-                    <div class="panel-body">
-                        <?=form_open('login')?>
-                            <fieldset>
-                              <?= $this->session->flashdata('msg') ?>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Username" name="username" type="text" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <input type="submit" class="btn btn-lg btn-success btn-block" name="login-submit" value="Masuk">
-                            </fieldset>
-                        <?=form_close()?>
-                    </div>
-                </div>
-            </div>
+    <div class ="container">
+        <div class="login">
+            <h1>Login</h1>
+            <?=form_open('login')?>
+                <fieldset>
+                  <?= $this->session->flashdata('msg') ?>
+                    <input type="text" name="u" placeholder="Username" required="required" />
+                    <input type="password" name="p" placeholder="Password" required="required" />
+                    <button type="submit" class="btn btn-primary btn-block btn-large">Masuk</button>
+                </fieldset>
+            <?=form_close()?>
         </div>
     </div>
 
