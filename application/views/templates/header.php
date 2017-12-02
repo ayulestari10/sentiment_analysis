483f44ef23736498ffcf6cb98939d314680b10cf
apriliaaachi <achiaprilia.aa@gmail.com>
2017-12-02 18:46:38 +0700

    tambah about

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 	<title><?= $title  ?> | Sentiment Analysis</title>
-	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/input/style.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>">
-	<script type="text/javascript" src="<?= base_url('assets/vendor/jquery/jquery.min.js') ?>"></script>
-	<script type="text/javascript" src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets')?>/css/input/style.css ">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/vendor')?>/bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="<?= base_url('assets/vendor') ?>/jquery/jquery.min.js"></script>
+	<script type="text/javascript" src="<?= base_url('assets/vendor') ?>/bootstrap/js/bootstrap.bundle.min.js"></script>
 	<!-- font -->
     <link rel="stylesheet" type="text/css" href="<?= base_url('assets') ?>/fonts/font-awesome/css/font-awesome.min.css">
 </head>
